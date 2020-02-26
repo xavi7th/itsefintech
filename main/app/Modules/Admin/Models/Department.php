6e68b30daee87843625f13ee4b23f667e34a7a45
leinad <xavi7th@gmail.com>
2020-02-26 09:35:53 +0100

    All departments view relevant tickets

--- main/app/Modules/Admin/Models/Department.php
+++ main/app/Modules/Admin/Models/Department.php
@@ -13,4 +13,39 @@ class Department extends Model
 	{
 		return $this->hasMany(SupportTicket::class);
 	}
+
+	static function salesRepsId()
+	{
+		return self::where('name', 'Sales Rep')->first()->id;
+	}
+
+	static function accountantsId()
+	{
+		return self::where('name', 'Accountant')->first()->id;
+	}
+
+	static function accountOfficersId()
+	{
+		return self::where('name', 'Account Officer')->first()->id;
+	}
+
+	static function adminId()
+	{
+		return self::where('name', 'Admin')->first()->id;
+	}
+
+	static function normalAdminId()
+	{
+		return self::where('name', 'Normal Admin')->first()->id;
+	}
+
+	static function cardAdminId()
+	{
+		return self::where('name', 'Card Admin')->first()->id;
+	}
+
+	static function customerSupportId()
+	{
+		return self::where('name', 'Customer Support')->first()->id;
+	}
 }
--- main/app/Modules/CustomerSupport/Models/SupportTicket.php
+++ main/app/Modules/CustomerSupport/Models/SupportTicket.php
@@ -39,6 +39,40 @@ class SupportTicket extends Model
 		return $this->belongsTo(Department::class);
 	}
 
+	public function scopeAccountants($query)
+	{
+		return $query->where('department_id', Department::accountantsId());
+	}
+
+	public function scopeAccountOfficers($query)
+	{
+		return $query->where('department_id', Department::accountOfficersId());
+	}
+
+	public function scopeAdmins($query)
+	{
+		return $query->where('department_id', Department::adminId());
+	}
+
+	public function scopeCardAdmins($query)
+	{
+		return $query->where('department_id', Department::cardAdminId());
+	}
+
+	public function scopeCustomerSupports($query)
+	{
+		return $query->where('department_id', Department::customerSupportId());
+	}
+
+	public function scopeNormalAdmins($query)
+	{
+		return $query->where('department_id', Department::normalAdminId());
+	}
+
+	public function scopeSalesReps($query)
+	{
+		return $query->where('department_id', Department::salesRepsId());
+	}
 
 	static function customerSupportRoutes()
 	{
@@ -50,7 +84,7 @@ class SupportTicket extends Model
 	static function adminRoutes()
 	{
 		Route::group(['namespace' => '\App\Modules\CustomerSupport\Models'], function () {
-			Route::get('support-tickets', 'SupportTicket@getSupportTickets')->middleware('auth:admin,sales_rep,card_admin,normal_admin,customer_support');
+			Route::get('support-tickets', 'SupportTicket@getSupportTickets')->middleware('auth:admin,sales_rep,card_admin,normal_admin,customer_support,accountant,account_officer');
 		});
 	}
 
@@ -60,7 +94,23 @@ class SupportTicket extends Model
 	 */
 	public function getSupportTickets()
 	{
-		return response()->json((new SupportTicketTransformer)->collectionTransformer(SupportTicket::withTrashed()->get(), 'transformForCustomerSupport'), 200);
+		if (auth('admin')->check()) {
+			return response()->json((new SupportTicketTransformer)->collectionTransformer(SupportTicket::withTrashed()->get(), 'transformForCustomerSupport'), 200);
+		} else if (auth('customer_support')->check()) {
+			return response()->json((new SupportTicketTransformer)->collectionTransformer(SupportTicket::withTrashed()->get(), 'transformForCustomerSupport'), 200);
+		} else if (auth('sales_rep')->check()) {
+			return response()->json((new SupportTicketTransformer)->collectionTransformer(SupportTicket::salesReps()->get(), 'transformForCustomerSupport'), 200);
+		} else if (auth('card_admin')->check()) {
+			return response()->json((new SupportTicketTransformer)->collectionTransformer(SupportTicket::cardAdmins()->get(), 'transformForCustomerSupport'), 200);
+		} else if (auth('normal_admin')->check()) {
+			return response()->json((new SupportTicketTransformer)->collectionTransformer(SupportTicket::normalAdmins()->get(), 'transformForCustomerSupport'), 200);
+		} else if (auth('accountant')->check()) {
+			return response()->json((new SupportTicketTransformer)->collectionTransformer(SupportTicket::accountants()->get(), 'transformForCustomerSupport'), 200);
+		} else if (auth('account_officer')->check()) {
+			return response()->json((new SupportTicketTransformer)->collectionTransformer(SupportTicket::accountOfficers()->get(), 'transformForCustomerSupport'), 200);
+		} else if (auth('normal_admin')->check()) {
+			return response()->json((new SupportTicketTransformer)->collectionTransformer(SupportTicket::normalAdmins()->get(), 'transformForCustomerSupport'), 200);
+		}
 	}
 
 
