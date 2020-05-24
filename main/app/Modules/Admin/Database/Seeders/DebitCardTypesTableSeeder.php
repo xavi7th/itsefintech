44ffd80e53128127da76dab58d4f9639e4e6f129
leinad <xavi7th@gmail.com>
2020-05-24 17:16:47 +0100

    Add new cardtype (virtual flutter) So that cards of this type can be created

--- main/app/Modules/Admin/Database/Seeders/DebitCardTypesTableSeeder.php
+++ main/app/Modules/Admin/Database/Seeders/DebitCardTypesTableSeeder.php
@@ -6,36 +6,45 @@ use Illuminate\Database\Seeder;
 class DebitCardTypesTableSeeder extends Seeder
 {
 
-	/**
-	 * Auto generated seed file
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
+  /**
+   * Auto generated seed file
+   *
+   * @return void
+   */
+  public function run()
+  {
 
 
-		\DB::table('debit_card_types')->delete();
+    \DB::table('debit_card_types')->delete();
 
-		\DB::table('debit_card_types')->insert(array(
-			0 =>
-			array(
-				'id' => 1,
-				'card_type_name' => 'Titanium Black',
-				'amount' => 1000,
-				'created_at' => '2020-01-16 04:51:47',
-				'updated_at' => '2020-01-16 04:51:47',
-				'deleted_at' => NULL,
-			),
-			1 =>
-			array(
-				'id' => 2,
-				'card_type_name' => 'Titanium Platinum',
-				'amount' => 1500,
-				'created_at' => '2020-01-16 04:55:06',
-				'updated_at' => '2020-01-16 04:55:06',
-				'deleted_at' => NULL,
-			),
-		));
-	}
+    \DB::table('debit_card_types')->insert(array(
+      0 =>
+      array(
+        'id' => 1,
+        'card_type_name' => 'Titanium Black',
+        'amount' => 1000,
+        'created_at' => '2020-01-16 04:51:47',
+        'updated_at' => '2020-01-16 04:51:47',
+        'deleted_at' => NULL,
+      ),
+      1 =>
+      array(
+        'id' => 2,
+        'card_type_name' => 'Titanium Platinum',
+        'amount' => 1500,
+        'created_at' => '2020-01-16 04:55:06',
+        'updated_at' => '2020-01-16 04:55:06',
+        'deleted_at' => NULL,
+      ),
+      2 =>
+      array(
+        'id' => 3,
+        'card_type_name' => 'Flutter Wave',
+        'amount' => 2500,
+        'created_at' => '2020-01-16 04:55:06',
+        'updated_at' => '2020-01-16 04:55:06',
+        'deleted_at' => NULL,
+      ),
+    ));
+  }
 }
