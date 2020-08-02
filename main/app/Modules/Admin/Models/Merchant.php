e11d112ab79cef9436615b03f072700011ffb721
leinad <xavi7th@gmail.com>
2020-08-02 03:54:38 +0100

    Fix: Card users still see suspended merchants

--- main/app/Modules/Admin/Models/Merchant.php
+++ main/app/Modules/Admin/Models/Merchant.php
@@ -56,108 +56,119 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
  */
 class Merchant  extends Model implements AuthenticatableContract, AuthorizableContract
 {
-	use Authenticatable, Authorizable;
+  use Authenticatable, Authorizable;
 
-	protected $fillable = [
-		'name', 'unique_code', 'email', 'phone', 'password', 'merchant_category_id', 'address', 'merchant_img'
-	];
+  protected $fillable = [
+    'name', 'unique_code', 'email', 'phone', 'password', 'merchant_category_id', 'address', 'merchant_img'
+  ];
 
-	const DASHBOARD_ROUTE_PREFIX = 'merchant-area';
+  const DASHBOARD_ROUTE_PREFIX = 'merchant-area';
 
 
-	public function activities()
-	{
-		return $this->morphMany(ActivityLog::class, 'user');
-	}
+  public function activities()
+  {
+    return $this->morphMany(ActivityLog::class, 'user');
+  }
 
-	public function merchant_transactions()
-	{
-		return $this->hasMany(MerchantTransaction::class);
-	}
+  public function merchant_transactions()
+  {
+    return $this->hasMany(MerchantTransaction::class);
+  }
 
-	public function merchant_category()
-	{
-		return $this->belongsTo(MerchantCategory::class);
-	}
+  public function merchant_category()
+  {
+    return $this->belongsTo(MerchantCategory::class);
+  }
 
-	static function cardUserRoutes()
-	{
-		Route::group(['namespace' => '\App\Modules\Admin\Models'], function () {
-			Route::get('merchant-list', 'Merchant@viewAllMerchants');
-		});
-	}
+  static function cardUserRoutes()
+  {
+    Route::group(['namespace' => '\App\Modules\Admin\Models'], function () {
+      Route::get('merchant-list', 'Merchant@viewAllMerchants');
+    });
+  }
 
-	static function adminRoutes()
-	{
-		Route::group(['namespace' => '\App\Modules\Admin\Models'], function () {
-			Route::get('merchants', 'Merchant@getAllMerchants')->middleware('auth:admin,normal_admin,account_officer');
-			Route::post('merchant/create', 'Merchant@createMerchant')->middleware('auth:admin,normal_admin');
-			Route::put('merchant/{merchant}/suspend', 'Merchant@suspendMerchant')->middleware('auth:admin,normal_admin');
-			Route::put('merchant/{merchant}/restore', 'Merchant@restoreMerchant')->middleware('auth:admin,normal_admin');
-		});
-	}
+  static function adminRoutes()
+  {
+    Route::group(['namespace' => '\App\Modules\Admin\Models'], function () {
+      Route::get('merchants', 'Merchant@getAllMerchants')->middleware('auth:admin,normal_admin,account_officer');
+      Route::post('merchant/create', 'Merchant@createMerchant')->middleware('auth:admin,normal_admin');
+      Route::put('merchant/{merchant}/suspend', 'Merchant@suspendMerchant')->middleware('auth:admin,normal_admin');
+      Route::put('merchant/{merchant}/restore', 'Merchant@restoreMerchant')->middleware('auth:admin,normal_admin');
+    });
+  }
 
-	/**
-	 * ! Card User Routes
-	 */
+  /**
+   * ! Card User Routes
+   */
 
-	public function viewAllMerchants()
-	{
-		return (new CardUserMerchantTransformer)->collectionTransformer(self::all(), 'transform');
-	}
+  public function viewAllMerchants()
+  {
+    return (new CardUserMerchantTransformer)->collectionTransformer(self::active()->orderBy('created_at')->get(), 'transform');
+  }
 
-	/**
-	 * ! Admin routes
-	 */
-	public function getAllMerchants()
-	{
-		return collect((new AdminMerchantTransformer)->collectionTransformer(self::all(), 'transformForAdminViewMerchants'))
-			->merge((new AdminMerchantCategoryTransformer)->collectionTransformer(MerchantCategory::all(), 'transformForAdminViewMerchantCategories'));
-	}
+  /**
+   * ! Admin routes
+   */
+  public function getAllMerchants()
+  {
+    return collect((new AdminMerchantTransformer)->collectionTransformer(self::all(), 'transformForAdminViewMerchants'))
+      ->merge((new AdminMerchantCategoryTransformer)->collectionTransformer(MerchantCategory::all(), 'transformForAdminViewMerchantCategories'));
+  }
 
-	public function createMerchant(CreateMerchantValidation $request)
-	{
+  public function createMerchant(CreateMerchantValidation $request)
+  {
 
-		if ($request->auto_generate) {
-			$prefix = strtoupper(Str::slug(substr($request->name, 0, 4))) . '-';
-			$merchant_id = unique_random('merchants', 'unique_code', $prefix, 8);
+    if ($request->auto_generate) {
+      $prefix = strtoupper(Str::slug(substr($request->name, 0, 4))) . '-';
+      $merchant_id = unique_random('merchants', 'unique_code', $prefix, 8);
 
-			$url = request()->file('merchant_img')->store('public/merchant_img');
-			// Storage::setVisibility($url, 'public');
+      $url = request()->file('merchant_img')->store('public/merchant_img');
+      // Storage::setVisibility($url, 'public');
 
-			/** Replace the public part of the url with storage to make it accessible on the frontend */
-			$url = Str::replaceFirst('public', '/storage', $url);
+      /** Replace the public part of the url with storage to make it accessible on the frontend */
+      $url = Str::replaceFirst('public', '/storage', $url);
 
-			$merchant = Merchant::create(Arr::add(Arr::add($request->except('unique_code'), 'unique_code', $merchant_id), 'merchant_img', $url));
-		} else {
-			$merchant = Merchant::create(Arr::add($request->all(), 'merchant_img', $url));
-		}
-		$merchant->is_active = true;
-		$merchant->merchant_img = $url;
-		$merchant->save();
+      $merchant = Merchant::create(Arr::add(Arr::add($request->except('unique_code'), 'unique_code', $merchant_id), 'merchant_img', $url));
+    } else {
+      $merchant = Merchant::create(Arr::add($request->all(), 'merchant_img', $url));
+    }
+    $merchant->is_active = true;
+    $merchant->merchant_img = $url;
+    $merchant->save();
 
-		ActivityLog::logAdminActivity(auth()->user()->email . ' created merchant ' . $merchant->name);
+    ActivityLog::logAdminActivity(auth()->user()->email . ' created merchant ' . $merchant->name);
 
-		return response()->json(['merchant' => $merchant], 201);
-	}
+    return response()->json(['merchant' => $merchant], 201);
+  }
 
-	public function suspendMerchant(Merchant $merchant)
-	{
-		$merchant->is_active = false;
-		$merchant->save();
+  public function suspendMerchant(Merchant $merchant)
+  {
+    $merchant->is_active = false;
+    $merchant->save();
 
-		ActivityLog::logAdminActivity(auth()->user()->email . ' suspended merchant ' . $merchant->name);
+    ActivityLog::logAdminActivity(auth()->user()->email . ' suspended merchant ' . $merchant->name);
 
-		return response()->json(['merchant' => $merchant], 204);
-	}
+    return response()->json(['merchant' => $merchant], 204);
+  }
 
-	public function restoreMerchant(Merchant $merchant)
-	{
-		$merchant->is_active = true;
-		$merchant->save();
+  public function restoreMerchant(Merchant $merchant)
+  {
+    $merchant->is_active = true;
+    $merchant->save();
 
-		ActivityLog::logAdminActivity(auth()->user()->email . ' restored ' . $merchant->name . '\'s account');
+    ActivityLog::logAdminActivity(auth()->user()->email . ' restored ' . $merchant->name . '\'s account');
 
-		return response()->json(['merchant' => $merchant], 204);
-	}
+    return response()->json(['merchant' => $merchant], 204);
+  }
+
+  /**
+   * Scope a query to only include active users.
+   *
+   * @param  \Illuminate\Database\Eloquent\Builder  $query
+   * @return \Illuminate\Database\Eloquent\Builder
+   */
+  public function scopeActive($query)
+  {
+    return $query->where('is_active', 1);
+  }
 }
